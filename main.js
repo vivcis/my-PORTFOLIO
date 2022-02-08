2541a6d42fabe34e57c5ececc6a48364e915d8b6
Decagon <decagon@Decagons-MacBook-Air.local>
2022-02-09 00:50:20 +0100

    added contact form functionalities

--- main.js
+++ main.js
@@ -10,3 +10,15 @@
        	    	document.getElementById("navbar").style.backgroundColor = "#000";	
        	    	}
        	    }
+
+			   function sendMail() {
+
+				const subject = document.getElementById('subject'.value);
+				
+				const name = document.getElementById('exampleFormControlInput1').value;
+				
+				const body = document.getElementById('exampleFormControlTextarea1').value;
+				
+				window.open(`mailto: ceciliaorji@yahoo.com.com?subject=${subject}&body=${body}`);
+				
+				}
